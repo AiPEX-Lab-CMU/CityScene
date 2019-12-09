7867dd9dc8d1d6a435ea73fd8599fdd076cc7440
Artificial Intelligence in Products Engineered for X (AiPEX) <aipexlab@gmail.com>
2019-12-09 12:24:08 -0500

    Add files via upload

---
+++ Assets/8K Skybox Pack Free/ReadMe.txt
@@ -0,0 +1,11 @@
+-----------------------
+8K Skybox Pack Free
+-----------------------
+
+To test the skyboxes please simply open the sample scenes in the folder "Example Scenes".
+
+Please note:
+* On Android platform, the maximum allowed cubemap size is 2048.
+* On iOS platform, the maximum allowed cubemap size is 4096.
+* In this package, the "max size" attribute of all cubmap textures is set to 2048 by default.  If you want to use the skyboxes on PC platform or iOS platform, please change the "max size" attribute of the cubemap textures to 4096. 
+
---
+++ Assets/manMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class manMovement : MonoBehaviour
+{
+    float speed = 0.3f;
+    int a = 0;
+    Vector3 moveDir = Vector3.zero;
+    Animator anim;
+    CharacterController controller;
+    static System.Random r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        anim.SetInteger("condition", 1);
+        Debug.Log("yo");
+        r = new System.Random();
+    }
+
+    IEnumerator Example()
+    {
+        int x = r.Next(10);
+        yield return new WaitForSeconds(5);
+        a = 1;
+        anim.SetInteger("condition", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(a==0)
+        {
+            StartCoroutine(Example());
+        }
+        
+        moveDir = new Vector3(0, 0, a);
+        moveDir *= speed;
+        moveDir = transform.TransformDirection(moveDir);
+        controller.Move(moveDir * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "roads_motorway.001")
+            Debug.Log("hi");
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.name == "roads_motorway.001")
+        {
+            hit.collider.isTrigger = true;
+            //a *= -1;
+            transform.Rotate(new Vector3(0, (transform.rotation.y + 180), 0));
+            anim.SetInteger("condition", 0);
+            a = 0;
+            hit.collider.isTrigger = false;
+        }
+    }
+}
